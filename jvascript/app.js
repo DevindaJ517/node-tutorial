3f0cec60f1271705588bdbc859fdbe85d9ebc3fa
Devinda J <168094003+DevindaJ517@users.noreply.github.com>
2025-09-05 05:26:38 +0000

    http server make

---
+++ jvascript/app.js
@@ -0,0 +1,41 @@
+// const Ramdom123_$ = `ramdom's`;
+// let tttt = "pppp";
+// const ttt = "dijdbnd";
+
+// console.log("hellow there your full name is "+ Ramdom123_$+"  "+ttt);
+// console.log(Ramdom123_$);
+
+// const webSite = "google";
+// const url = "https://www."+ webSite+ ".com";
+// console.log(url);
+
+// const num1 = 1234;
+// let pants = "2.456";
+
+// const num2 = 2.456;
+
+// const add = num1 + num2;
+// const sub = num1 - num2;
+// const div = num1 / num2;
+// const mul = num1 * num2;
+
+// const slices = (4 + 6 + 10) * 10;
+
+// console.log(num1);
+// console.log(pants);
+// console.log(add);
+// console.log(sub);
+// console.log(mul);
+// console.log(div);
+// console.log(slices);
+
+const text = ['aaa', 'www', 123,'hhhh', 'gggg'];
+let top1 = text[2];
+text[1]= 1234
+
+
+console.log(text);
+
+
+
+
